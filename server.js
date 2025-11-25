55cd8bac5c478d43adaf80fe8e057032d97e45fc
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-25 12:20:23 +0100

    export posts.js from routers and import in server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const postsRouter = require('./routers/posts')
 
 app.use(express.static('public'))
+
+app.use('/posts', postsRouter)
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
