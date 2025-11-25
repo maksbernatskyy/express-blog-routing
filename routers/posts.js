dd0228e3c8c714f047cb8cb9deecbbbbb8d20ab1
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-25 12:50:43 +0100

    add actions whit router

--- routers/posts.js
+++ routers/posts.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const postsArr = require('../public/postsArr')
 
+// index
+router.get('/', (req, res) => {
+    res.send('Show all posts')
+})
+
+// show
+router.get('/:id', (req, res) => {
+    res.send(`Show the post with id: ${req.params.id}`)
+})
+
+// delete
+router.delete('/:id', (req, res) => {
+    res.send(`Delete the post with id: ${req.params.id}`)
+})
+
+// Export the router
 module.exports = router
