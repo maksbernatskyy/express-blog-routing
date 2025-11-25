49399d3e04b87c3a77ca58aa804e282e3c628793
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-25 11:35:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Example app on port ${PORT}`)
+})
